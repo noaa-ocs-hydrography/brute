920f8c1e0eb0b6c77c299101f0269fe79882eb19
juliet.kinney <juliet.kinney@noaa.gov>
2019-04-09 17:02:29 -0400

    edit bug fix saved parse_usace_xml.py

--- fuse_dev/fuse/raw_read/usace/parse_usace_xml.py
+++ fuse_dev/fuse/raw_read/usace/parse_usace_xml.py
@@ -1236,35 +1236,39 @@ def parse_xml_info_text_ISO(xml_txt, m):
 
 def extract_from_iso_meta(xml_meta):
     xml_meta['from_vert_units'] = xml_meta['altunits']
-    Hor_unc = xml_meta['Implied_Horizontal_Accuracy']
-    Vert_unc = xml_meta['Implied_Vertical_Accuracy']
-    Hor_unc = Hor_unc.strip('+/- ')
-    Vert_unc = Vert_unc.strip('+/- ')
-    if Hor_unc.find('feet'):
-        Hor_unc = Hor_unc.rstrip('Feet')
-        Hor_unc = float(Hor_unc)
-        Hor_unc = Hor_unc * _ussft2m
-        xml_meta['from_horiz_unc'] = str(Hor_unc)
-    if Vert_unc.find('feet'):
-        Vert_unc = Vert_unc.rstrip('Feet')
-        Vert_unc = float(Vert_unc)
-        Vert_unc = Vert_unc * _ussft2m
-        xml_meta['from_vert_unc'] = str(Vert_unc)
-    if xml_meta['System'] == 'single beam':
-        xml_meta['TECSOU'] = '1'
-    elif  xml_meta['System'] == 'multibeam beam':
-        xml_meta['TECSOU'] = '3'
-    elif  xml_meta['System'].find('sweep'):
-        xml_meta['TECSOU'] = ''
-    xml_meta ['from_horiz_datum'] = xml_meta['Projected_Coordinate_System'] + ',' + xml_meta['Horizontal_Zone'] + ',' + xml_meta['Units']
-    xml_meta ['from_horiz_units'] = xml_meta['Units']
-    if len(xml_meta['Horizontal_Zone']) >0 :        
-        code = xml_meta['Horizontal_Zone'].split(' ')[1]
-        print(code)
-        for key in SOURCEPROJECTION_dict:
-            if key.upper() in xml_meta['Horizontal_Zone']:
-                print(key)
-                xml_meta['from_fips'] = convert_tofips(SOURCEPROJECTION_dict, key)
+    if 'altdatum' in xml_meta:
+        xml_meta['from_vert_datum'] =xml_meta['altdatum']
+    xml_meta['from_vert_units'] = xml_meta['altunits']
+    if 'Implied_Horizontal_Accuracy' in xml_meta:
+        Hor_unc = xml_meta['Implied_Horizontal_Accuracy']
+        Vert_unc = xml_meta['Implied_Vertical_Accuracy']
+        Hor_unc = Hor_unc.strip('+/- ')
+        Vert_unc = Vert_unc.strip('+/- ')
+        if 'Feet' in Hor_unc:
+            Hor_unc = Hor_unc.rstrip('Feet')
+            Hor_unc = float(Hor_unc)
+            Hor_unc = Hor_unc * _ussft2m
+            xml_meta['from_horiz_unc'] = str(Hor_unc)
+        if 'Feet' in Vert_unc:
+            Vert_unc = Vert_unc.rstrip('Feet')
+            Vert_unc = float(Vert_unc)
+            Vert_unc = Vert_unc * _ussft2m
+            xml_meta['from_vert_unc'] = str(Vert_unc)
+        if xml_meta['System'] == 'single beam':
+            xml_meta['TECSOU'] = '1'
+        elif  xml_meta['System'] == 'multibeam beam':
+            xml_meta['TECSOU'] = '3'
+        elif  xml_meta['System'].find('sweep'):
+            xml_meta['TECSOU'] = ''
+        xml_meta ['from_horiz_datum'] = xml_meta['Projected_Coordinate_System'] + ',' + xml_meta['Horizontal_Zone'] + ',' + xml_meta['Units']
+        xml_meta ['from_horiz_units'] = xml_meta['Units']
+        if len(xml_meta['Horizontal_Zone']) >0 :        
+            code = xml_meta['Horizontal_Zone'].split(' ')[1]
+            print(code)
+            for key in SOURCEPROJECTION_dict:
+                if key.upper() in xml_meta['Horizontal_Zone']:
+                    print(key)
+                    xml_meta['from_fips'] = convert_tofips(SOURCEPROJECTION_dict, key)
     return xml_meta
                     
 def convert_meta_to_input(m):
