25637ff104a0119be204b3dcd06c3a5561b8cba6
Glen Rice <glen.rice@noaa.gov>
2019-05-08 17:18:39 -0400

    Rearranged the workflow to simplify.

--- fuse_dev/fuse/proc_io/proc_io.py
+++ fuse_dev/fuse/proc_io/proc_io.py
@@ -12,7 +12,7 @@ import subprocess
 from tempfile import TemporaryDirectory as tempdir
 import logging
 import numpy as np
-from osgeo import gdal, osr
+from osgeo import gdal
 gdal.UseExceptions()
 
 __version__ = 'Test'
@@ -35,12 +35,15 @@ class proc_io:
     """
     A class to abstract the reading and writing of bathymetry.
     """
-    def __init__(self, in_data_type, out_data_type, work_dir = None):
+    def __init__(self, in_data_type, out_data_type, work_dir = None,
+                 z_up = True, nodata=100000.0):
         """
         Initialize with the data type to be worked.
         """
         self._in_data_type = in_data_type
         self._out_data_type = out_data_type
+        self._z_up = z_up
+        self._write_nodata = nodata
         if work_dir == None:
             self._work_dir = tempdir()
             self._work_dir_name = self._work_dir.name
@@ -48,79 +51,33 @@ class proc_io:
             self._work_dir_name = work_dir
         self._logger = logging.getLogger('fuse')
 
-    def write(self, dataset, outfilename, z_up = True, nodata=100000.0):
+    def write(self, dataset, outfilename):
         """
         Write the provided data to the predefined data type.
         """
-        if self._in_data_type =='gdal':
-            data, metadata = self._convert_gdal(dataset)
-        elif self._in_data_type =='bag':
-            dataset, metadata = self._grab_gdal(dataset, nodata)
-        else:
-            raise ValueError('input data type unknown: ' +
-                             str(self._in_data_type))
-        metadata['outfilename'] = outfilename
-        metadata['z_up'] = z_up
         if self._out_data_type == 'csar':
-            self._write_csar(data, metadata)
+            self._write_csar(dataset, outfilename)
         elif self._out_data_type == 'bag':
-            self._write_bag(dataset, metadata, nodata)
+            self._write_bag(dataset, outfilename)
         else:
             raise ValueError('writer type unknown: ' +
                              str(self._out_data_type))
-
-    def _convert_gdal(self, dataset):
+            
+    def _write_csar(self, dataset, outfilename, conda_env_name = 'NBS35'):
         """
-        Convert the gdal dataset into a numpy array and a dictionary and
-        return.
-        """
-        meta = {}
-        # get the logisitics for converting the gdal dataset to csar
-        gt = dataset.GetGeoTransform()
-        print (gt)
-        meta['resx'] = gt[1]
-        meta['resy'] = gt[1]
-        meta['originx'] = gt[0]
-        meta['originy'] = gt[3] - dataset.RasterYSize
-        meta['dimx'] = dataset.RasterXSize
-        meta['dimy'] = dataset.RasterYSize
-        print (meta)
-        meta['crs'] = dataset.GetProjection()
-        rb = dataset.GetRasterBand(1) # should this be hardcoded for 1?
-#        meta['nodata'] = rb.GetNoDataValue()
-        # get the gdal data raster
-        data = np.flipud(rb.ReadAsArray())
-        maxVal = maxValue(data)
-        meta['nodata'] = maxVal
-        return data, meta
-
-    def _grab_gdal(self, dataset, nodata):
-        """
-        Convert the gdal dataset into a numpy array and a dictionary and
-        return.
-        """
-        meta = {}
-        # get the logisitics for converting the gdal dataset to csar
-        gt = dataset.GetGeoTransform()
-        meta['gt'] = gt
-        rb = dataset.GetRasterBand(1) # should this be hardcoded for 1?
-        # get the gdal data raster
-        data = rb.ReadAsArray()
-        print (meta)
-        maxVal = maxValue(data)
-        if maxVal != nodata:
-            data = np.where(data==maxVal, nodata, data)
-            dataset.GetRasterBand(1).WriteArray(data)
-        return dataset, meta
-
-    def _write_csar(self, data, metadata, conda_env_name = 'NBS35'):
-        """
-        Convert the provided numpy array into a csar file using the provided
-        metadata.
+        Convert the provided gdal dataset into a csar file.
 
         The data and metadata are saved out to a file and then loaded into the
         wrapper around the csar writer.
         """
+        # put the provided data into the right form for the csar conversion.
+        if self._in_data_type =='gdal':
+            data, metadata = self._gdal2array(dataset)
+        else:
+            raise ValueError('input data type unknown: ' +
+                             str(self._in_data_type))
+        metadata['outfilename'] = outfilename
+        metadata['z_up'] = self._z_up
         conda_env_path = _retrieve_env_path(conda_env_name)
         python_path = os.path.join(conda_env_path, 'python')
         # save the provided dataset and metadata to a file
@@ -161,47 +118,62 @@ class proc_io:
         else:
             print("Unable to create %s" % metadata['outfilename'])
 
-    def _write_bag(self, dataset, metadata, dtype=gdal.GDT_UInt32,
-                 options=0, color_table=0, nbands=1, nodata=False):
-        """Directly From:
-        "What is the simplest way..." on GIS Stack Exchange [Answer by 'Jon'
-        (https://gis.stackexchange.com/a/278965)]
-
+    def _write_bag(self, dataset, outfilename):
+        """
         Parameters
         ----------
-        raster_array : numpy.array
-            Array to be written to a GeoTiff file
-        gt : tuple, gdal.GeoTransform
-            Norhtern extent, resolution, 0.0, Western extent, 0.0, -resolution)
-        data_obj : gdal.RasterBand
-            gdal.RasterBand
-        outputpath : string
-            Folder to save the GeoTiff raster
-
+        dataset : A georeferenced gdal raster object.
+        
+        outfilename : A string defining the path and filename of the file to be
+            written.
         """
-        print('_write_bag')
-        print (metadata)
-
+        if self._in_data_type =='gdal':
+            pass
+        else:
+            raise ValueError('input data type unknown: ' +
+                             str(self._in_data_type))
+        # check the no data value
+        rb = dataset.GetRasterBand(1) # should this be hardcoded for 1?
+        ndv = rb.GetNoDataValue()
+        if self._write_nodata != ndv:
+            data = rb.ReadAsArray()
+            data = np.where(data==ndv, self._write_nodata, data)
+            dataset.GetRasterBand(1).WriteArray(data)
         # Prepare destination file
         driver = gdal.GetDriverByName("BAG")
-        if os.path.exists(metadata['outfilename']):
-            os.remove(metadata['outfilename'])
-            os.remove(metadata['outfilename']+'.aux.xml')
-        if options != 0:
-            dest = driver.CreateCopy(metadata['outfilename'], dataset)
-        else:
-            dest = driver.CreateCopy(metadata['outfilename'], dataset)
-
-        # Write output raster
-        if color_table != 0:
-            dest.GetRasterBand(1).SetColorTable(color_table)
-
-        if nodata is not False:
-            dest.GetRasterBand(1).SetNoDataValue(nodata)
-
-        # Close output raster dataset
+        if os.path.exists(outfilename):
+            os.remove(outfilename)
+            os.remove(outfilename +'.aux.xml')
+        # write and close output raster dataset
+        dest = driver.CreateCopy(outfilename, dataset)
+        dest.GetRasterBand(1).SetColorTable(0)
         dest = None
-        print ('it is written')
+        self._logger.log(logging.DEBUG, 'BAG file created')
+
+    def _gdal2array(self, dataset):
+        """
+        Convert the gdal dataset into a numpy array and a dictionary of
+        metadata of the geotransform information and return.
+        """
+        meta = {}
+        # get the logisitics for converting the gdal dataset to csar
+        gt = dataset.GetGeoTransform()
+        print (gt)
+        meta['resx'] = gt[1]
+        meta['resy'] = gt[1]
+        meta['originx'] = gt[0]
+        meta['originy'] = gt[3] - dataset.RasterYSize
+        meta['dimx'] = dataset.RasterXSize
+        meta['dimy'] = dataset.RasterYSize
+        print (meta)
+        meta['crs'] = dataset.GetProjection()
+        rb = dataset.GetRasterBand(1) # should this be hardcoded for 1?
+#        meta['nodata'] = rb.GetNoDataValue()
+        # get the gdal data raster
+        data = np.flipud(rb.ReadAsArray())
+        maxVal = maxValue(data)
+        meta['nodata'] = maxVal
+        return data, meta
 
     def _get_logfilename(self):
         """
@@ -234,6 +206,8 @@ class proc_io:
         # create file handler for this filename
         fh = logging.FileHandler(handlefilename)
         fh.setLevel(logging.DEBUG)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        fh.setFormatter(formatter)
         self._logger.addHandler(fh)
 
 # helper function to retrieve the path to the "Scripts" folder in PydroXL
