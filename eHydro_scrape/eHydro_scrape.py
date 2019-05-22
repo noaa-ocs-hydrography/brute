8ec7b767b840d76d4340e46d07fdb0b2bcf2ee97
Casiano Koprowski <Casiano.Koprowski@noaa.gov>
2019-05-22 11:53:29 -0400

    eHydro_srape - Uncomment of code for testing

--- eHydro_scrape/eHydro_scrape.py
+++ eHydro_scrape/eHydro_scrape.py
@@ -933,25 +933,25 @@ def main(pb=None,to=None):
     elif runType == 'yes':
         csvFile = []
         changes = rows
-#    try:
-    logWriter(fileLog, '\tParsing new entries for resolution:')
-    attributes.append('Hi-Res?')
-    attributes.append('Override?')
-    if changes != 'No Changes':
-        checked, hiRes = downloadAndCheck(changes, pb, to)
-        csvFile.extend(checked)
-        if config['Output Log']['Query List'] == 'yes':
-            logWriter(fileLog, '\tNew Survey Details:')
-            for row in checked:
-                txt = ''
-                for i in [1,4,5,6,-2]:
-                    txt = txt + attributes[i] + ' : ' + row[i] + '\n\t\t'
-                logWriter(fileLog, '\t\t' + txt)
-        logWriter(fileLog, '\t\tTotal High Resloution Surveys: ' + str(hiRes) + '/' + str(len(changes)) + '\n')
-    else:
-        logWriter(fileLog, '\t\t' + changes)
-#    except:
-#        logWriter(fileLog, '\tParsing for resolution failed')
+    try:
+        logWriter(fileLog, '\tParsing new entries for resolution:')
+        attributes.append('Hi-Res?')
+        attributes.append('Override?')
+        if changes != 'No Changes':
+            checked, hiRes = downloadAndCheck(changes, pb, to)
+            csvFile.extend(checked)
+            if config['Output Log']['Query List'] == 'yes':
+                logWriter(fileLog, '\tNew Survey Details:')
+                for row in checked:
+                    txt = ''
+                    for i in [1,4,5,6,-2]:
+                        txt = txt + attributes[i] + ' : ' + row[i] + '\n\t\t'
+                    logWriter(fileLog, '\t\t' + txt)
+            logWriter(fileLog, '\t\tTotal High Resloution Surveys: ' + str(hiRes) + '/' + str(len(changes)) + '\n')
+        else:
+            logWriter(fileLog, '\t\t' + changes)
+    except:
+        logWriter(fileLog, '\tParsing for resolution failed')
     try:
         csvFile.insert(0, attributes)
         csvSave = csvFile
