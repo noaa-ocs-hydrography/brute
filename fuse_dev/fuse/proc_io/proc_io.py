484a4d9c2f09d50dbeed22827e89bc317a8f5f27
Glen Rice <glen.rice@noaa.gov>
2019-05-29 16:22:46 -0400

    Removing methods for finding resources for starting other environments to an external module and placing the module in the caris folder.

--- fuse_dev/fuse/proc_io/proc_io.py
+++ fuse_dev/fuse/proc_io/proc_io.py
@@ -14,6 +14,7 @@ import logging
 import numpy as np
 from osgeo import gdal
 gdal.UseExceptions()
+from fuse.proc_io.caris import helper
 
 __version__ = 'Test'
 
@@ -82,7 +83,7 @@ class proc_io:
                              str(self._in_data_type))
         metadata['outfilename'] = outfilename
         metadata['z_up'] = self._z_up
-        conda_env_path = _retrieve_env_path(conda_env_name)
+        conda_env_path = helper.retrieve_env_path(conda_env_name)
         python_path = os.path.join(conda_env_path, 'python')
         # save the provided dataset and metadata to a file
         datafilename = os.path.join(self._work_dir_name, 'rasterdata.npy')
@@ -92,8 +93,8 @@ class proc_io:
             pickle.dump(metadata, metafile)
         # set the locations for running the wrap_csar script
         start = os.path.realpath(os.path.dirname(__file__))
-        write_csar = os.path.join(start, 'wrap_csar.py')
-        activate_file = _retrieve_activate_batch()
+        write_csar = os.path.join(start, 'caris','wrap_csar.py')
+        activate_file = helper.retrieve_activate_batch()
         if os.path.exists(write_csar):
             args = ["cmd.exe", "/K", "set pythonpath= &&", # setup the commandline
                     activate_file, conda_env_name, "&&",  # activate the Caris 3.5 virtual environment
@@ -193,31 +194,3 @@ class proc_io:
             data = np.where(data==ndv, self._write_nodata, data)
             dataset.GetRasterBand(1).WriteArray(data)
         return dataset
-
-# helper function to retrieve the path to the "Scripts" folder in PydroXL
-def _retrieve_scripts_folder():
-    install_prefix = sys.exec_prefix
-    folder_path = os.path.realpath(os.path.join(install_prefix, os.pardir, os.pardir, "Scripts"))
-    if not os.path.exists(folder_path):
-        raise RuntimeError("The Scripts folder does not exist at: %s" % folder_path)
-    return folder_path
-
-# helper function to retrieve the path to the "activate.bat" batch file in PydroXL
-def _retrieve_activate_batch():
-
-    scripts_prefix = _retrieve_scripts_folder()
-    file_path = os.path.realpath(os.path.join(scripts_prefix, "activate.bat"))
-    if not os.path.exists(file_path):
-        raise RuntimeError("The activate file does not exist at: %s" % file_path)
-    return file_path
-
-def _retrieve_env_path(env_name):
-    """
-    Given a conda environement name, find the environment.
-    """
-    current_env_loc = os.environ['conda_prefix']
-    desired_env_loc = os.path.join(current_env_loc, os.pardir, env_name)
-    if os.path.exists(desired_env_loc):
-        return desired_env_loc
-    else:
-        raise ValueError('{} environment does not exist in current conda installation'.format(env_name))
