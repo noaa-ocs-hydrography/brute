a1e54243e642c5b917d26b002c2be8bc87700788
Casiano Koprowski <Casiano.Koprowski@noaa.gov>
2019-06-11 15:08:04 -0400

    fuse/interpolator/bag_interpolator/bag.py - Ensured that the bag_file object could be initialized as an empty data object. Added class gdal_crate in order to facilitate the posiblility of building a new gdal dataset to hold bag data from either the bag_file class or individual components fuse/interpolator/bag_interpolator/coverage.py - Ensured that the geotiff and geopackage objects could be initialized as an empty data objects.  Added class unified_coverage; this data class is meant to process and align input coverage data. Function align2grid() helps align a coverage object to a given extent, shape, and resolutiuon. Function write_raster() has been modified to accept a coverage object fuse/interpolator/bag_interpolator/interpolator.py - Streamlined the interpolation process by re-introducing sliceFinder(), chunk(), and class tile.  These items help manage and define how arrays are individually processed fuse/interpolator/bag_interpolator/proces.py -  process.py class initialize represents the main function of bag_interpolator.  It handles data ingest and moves the data through the tiled interpolation process.  It currently handles both input and output data
    This commit refs #64731

--- fuse_dev/fuse/interpolator/bag_interpolator/bag.py
+++ fuse_dev/fuse/interpolator/bag_interpolator/bag.py
@@ -6,8 +6,11 @@ Created on Thu Jun  6 15:29:05 2019
 """
 import os as _os
 import numpy as _np
-from hyo2 import bag as _bag
 
+from hyo2 import bag as _bag
+from osgeo import gdal as _gdal
+
+_gdal.UseExceptions()
 
 class bag_file:
     """This class serves as the main container for BAG data.
@@ -15,11 +18,6 @@ class bag_file:
     In order to read and assign the data relevant to the interpolation process,
     this class heavily depends on HydrOffice's hyo2.bag module to open and
     populate the data needed
-
-    Parameters
-    ----------
-    filepath : str
-        The complete file path of the input BAG file
 
     Attributes
     ----------
@@ -37,7 +35,26 @@ class bag_file:
         The WKT representation of the data CRS
 
     """
-    def __init__(self, filepath):
+    def __init__(self):
+        self.name = None
+        self.nodata = 1000000.0
+        self.elevation = None
+        self.uncertainty = None
+        self.shape = None
+        self.bounds = None
+        self.resolution = None
+        self.wkt = None
+        self.size = None
+        self.outfilename = None
+
+    def open_file(self, filepath):
+        """
+        Parameters
+        ----------
+        filepath : str
+            The complete file path of the input BAG file
+
+        """
         _fName = _os.path.split(filepath)[-1]
         self.name = _os.path.splitext(_fName)[0]
         self.nodata = 1000000.0
@@ -49,9 +66,11 @@ class bag_file:
         self.bounds = self._meta2bounds(_bag_obj.meta)
         self.resolution = (_bag_obj.meta.res_x, _bag_obj.meta.res_y)
         self.wkt = _bag_obj.meta.wkt_srs
+        self.size = self._size_finder(filepath)
 
     def _nan2ndv(self, arr, nodata):
         arr[_np.isnan(arr)] = nodata
+        arr = _np.flipud(arr)
         return arr
 
     def _meta2bounds(self,meta):
@@ -59,3 +78,61 @@ class bag_file:
         nx,ny = meta.ne
         bounds = ([sx,ny],[nx,sy])
         return bounds
+
+    def _size_finder(self, filepath):
+        size = int(_np.round(_os.path.getsize(filepath)/1000))
+        return size
+
+    def generate_name(self, outlocation, io):
+        if io:
+            ext = '_INTERP_ONLY.bag'
+        else:
+            ext = '_INTERP_FULL.bag'
+        name = self.name + ext
+        self.outfilename = _os.path.join(outlocation, name)
+
+class gdal_create:
+    def __init__(self):
+        self.dataset = None
+
+    def bag2gdal(self, bag):
+        arrays = [bag.elevation, bag.uncertainty]
+        bands = len(arrays)
+        x_orig, y_orig = bag.bounds[0]
+        y_cols, x_cols = bag.shape
+        res_x, res_y = bag.resolution[0], bag.resolution[1]
+        target_ds = _gdal.GetDriverByName('MEM').Create('', x_cols, y_cols,
+                                                       bands, _gdal.GDT_Float32)
+        target_gt = (x_orig, res_x, 0, y_orig, 0, res_y)
+        target_ds.SetGeoTransform(target_gt)
+        target_ds.SetProjection(bag.wkt)
+        x = 1
+        for item in arrays:
+            band = target_ds.GetRasterBand(x)
+            band.SetNoDataValue(bag.nodata)
+            band.WriteArray(_np.flipud(item))
+            band = None
+            x+=1
+        self.dataset = target_ds
+        target_ds = None
+
+    def components2gdal(self, arrays, shape, bounds, resolution, wkt,
+                 nodata):
+        bands = len(arrays)
+        x_orig, y_orig = bounds[0]
+        y_cols, x_cols = shape
+        res_x, res_y = resolution[0], resolution[1]
+        target_ds = _gdal.GetDriverByName('MEM').Create('', x_cols, y_cols,
+                                                       bands, _gdal.GDT_Float32)
+        target_gt = (x_orig, res_x, 0, y_orig, 0, res_y)
+        target_ds.SetGeoTransform(target_gt)
+        target_ds.SetProjection(wkt)
+        x = 1
+        for item in arrays:
+            band = target_ds.GetRasterBand(x)
+            band.SetNoDataValue(nodata)
+            band.WriteArray(_np.flipud(item))
+            band = None
+            x+=1
+        self.dataset = target_ds
+        target_ds = None
