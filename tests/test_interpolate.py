0112668cbc51df4d88056041c761ec28a2d939f3
zacharyburnettNOAA <zachary.burnett@noaa.gov>
2019-07-25 08:49:40 -0400

    testing align2grid

--- tests/test_interpolate.py
+++ tests/test_interpolate.py
@@ -1,2 +1,9 @@
+import fuse.interpolator.bag_interpolator
+
 def test_interpolate():
     assert 1 == 1
+
+
+class TestBagInterpolator:
+    def test_align2grid(self):
+        fuse.interpolator.bag_interpolator.coverage.align2grid()
