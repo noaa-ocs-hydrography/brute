f0fdfe1ace5d148383e9aac46281309f5b9bca90
Glen Rice <glen.rice@noaa.gov>
2019-07-25 21:31:27 -0400

    Fill out the score module.

--- fuse_dev/fuse/score/score.py
+++ fuse_dev/fuse/score/score.py
@@ -18,22 +18,99 @@ def catzoc(metadata: dict) -> int:
     The enumeration (catzoc : value) is as follows:
         A1 : 1
         A2 : 2
-        A3 : 3
-        B  : 4
-        C  : 5
-        D  : 6
+        B  : 3
+        C  : 4
+        D  : 5
+        U  : 6
         
     The provided metadata is expected to contain the following metadata values:
         
     """
-    pass
+    s = supersession(metadata)
+    if s >= 100:
+        return 1
+    elif s >= 90:
+        return 2
+    elif s >= 80:
+        return 3
+    elif s >= 70:
+        return 4
+    elif s >= 60:
+        return 5
+    else:
+        return 6
 
 def supersession(metadata: dict) -> float:
     """
     Return the superssion score as defined in Wyllie 2017 at US Hydro for the
     catzoc score.
     """
-    pass
+    feat_score = _get_feature_detection(metadata)
+    cov_score = _get_coverage(metadata)
+    horz_score = _get_horizontal_uncertainty(metadata)
+    vert_score = _get_vertical_uncertainty
+    return min(feat_score, cov_score, horz_score, vert_score)
+
+def _get_feature_detection(metadata: dict) -> float:
+    """
+    Determine the feature detection capability from the ability to detect
+    features, detect the least depth, and the size of the feature.
+    """
+    detected = metadata['feat_detect'].upper() == 'TRUE'
+    least_depth = metadata['feat_least_depth'].upper() == 'TRUE'
+    size = float(metadata['feat_size'])
+    if size <= 2: 
+        size_okay = True
+    else:
+        size_okay = False
+    if detected and least_depth and size_okay:
+        return 100
+    else:
+        return 80
+    
+def _get_coverage(metadata: dict) -> float:
+    """
+    Determine the coverage score and return.
+    """
+    cov = metadata['complete_coverage'].upper() == 'TRUE'
+    if cov:
+        return 100
+    else:
+        return 80
+    
+def _get_horizontal_uncertainty(metadata: dict) -> float:
+    """
+    Determine the horizontal uncertainty score and return.
+    """
+    h_fix = float(['horiz_uncert_fixed'])
+    h_var = float(['horiz_uncert_vari'])
+    if h_fix <= 5 and h_var <= 0.05:
+        s = 100
+    elif h_fix <= 20:
+        s = 90
+    elif h_fix <= 50:
+        s = 80
+    elif h_fix <= 500: 
+        s = 70
+    else:
+        s = 60
+    return s
+
+def _get_vertical_uncertainty(metadata: dict) -> float:
+    """
+    Determine the vertical uncertainty score and return.
+    """
+    v_fix = float(metadata['vert_uncert_fixed'])
+    v_var = float(metadata['vert_uncert_vari'])
+    if v_fix <= 0.5 and v_var <= 0.01:
+        s = 100
+    elif v_fix <= 1 and v_var <= 0.02:
+        s = 90
+    elif v_fix <= 2 and v_var <= 0.05: 
+        s = 70
+    else:
+        s = 60
+    return s
 
 def decay(metadata: dict, date: datetime, alpha: float = 0.022 ) -> float:
     """
