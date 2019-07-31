558b57db3743436fdb020c6a5305281310b19903
zachary.burnett <0006534511801c48D15@noaa.gov>
2019-07-31 11:09:25 -0400

    tiling test works now

--- tests/test_tiling.py
+++ tests/test_tiling.py
@@ -1,3 +1,4 @@
+import os
 import unittest
 from tiling import tiling
 
@@ -5,15 +6,17 @@ from tiling import tiling
 class TestTiling(unittest.TestCase):
     def test_tiling(self):
         xy_level = '44'
+        test_point = (-70.87, 43.18)
         tiling.build_gpkg(xy_level, [-180, -90, 180, 90])
 
         assert tiling.get_tile_name(361) == '00000A1'
-        assert tiling.get_tile_from_point(xy_level, [-70.8749, 43.1889]) == (
-            [-70.6640625, 42.890625, -70.3125, 43.2421875], '00000A1')
+        assert tiling.get_tile_from_point(xy_level, test_point) == ([-78.75, 33.75, -67.5, 45.0], '00000A1')
 
-        assert tiling.get_tile_from_point('88', [70.5, 43]) == ([70.3125, 42.890625, 71.015625, 43.59375], '0000H2P')
-        assert tiling.get_shapely('99', [42.890625, 70.3125, 43.59375, 71.015625])[-1] == ['000A0SA', '000A0SB',
-                                                                                           '000A1KQ', '000A1KR']
+        assert tiling.get_tile_from_point('33', test_point) == ([-90.0, 22.5, -67.5, 45.0], '000002C')
+        assert tiling.get_shapely(xy_level, [-90.0, 22.5, -67.5, 45.0])[-1] == ['0000094', '0000095', '00000A0',
+                                                                                '00000A1']
+
+        # os.remove(os.path.join(os.curdir, f'{xy_level}_tesselation.gpkg'))
 
 
 if __name__ == '__main__':
