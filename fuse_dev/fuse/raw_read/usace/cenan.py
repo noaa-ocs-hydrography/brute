b57ff89a771454b87163b5ecb9c0c0a475502beb
Glen Rice <glen.rice@noaa.gov>
2019-08-06 16:18:23 -0400

    Reported uncertainties should be in survey datum until converted in datum transformation.

--- fuse_dev/fuse/raw_read/usace/cenan.py
+++ fuse_dev/fuse/raw_read/usace/cenan.py
@@ -24,9 +24,6 @@ from fuse.datum_transform import usefips as _usefips
 
 class CENANRawReader:
     """An abstract raw data reader."""
-
-    _ussft2m = 0.30480060960121924  # US survey feet to meters
-
     def __init__(self):
         """
         No init needed?
@@ -135,11 +132,11 @@ class CENANRawReader:
         merged_meta = {**default_meta, **name_meta, **file_meta}
         if 'from_horiz_unc' in merged_meta:
             if merged_meta['from_horiz_units'] == 'US Survey Foot':
-                val = CENANRawReader._ussft2m * float(merged_meta['from_horiz_unc'])
+                val = float(merged_meta['from_horiz_unc'])
                 merged_meta['horiz_uncert'] = val
         if 'from_vert_unc' in merged_meta:
             if merged_meta['from_vert_units'] == 'US Survey Foot':
-                val = CENANRawReader._ussft2m * float(merged_meta['from_vert_unc'])
+                val = float(merged_meta['from_vert_unc'])
                 merged_meta['vert_uncert_fixed'] = val
                 merged_meta['vert_uncert_vari'] = 0
         sorind = f"{name_meta['projid']}_{name_meta['uniqueid']}_{name_meta['subprojid']}_{name_meta['start_date']}_" + \
