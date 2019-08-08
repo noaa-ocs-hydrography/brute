da32d68863817e464ca663b7679c7e6b10afde69
juliet.kinney <juliet.kinney@noaa.gov>
2019-08-08 17:45:56 -0400

    creating new scripts to gulf districts for reprocess, testing post & score. Saving edits thus far.

---
+++ fuse_dev/scripts/score/fuse_score_cesaj.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+fuse_score_cesaj.py
+
+grice
+20190725
+V.0.0.1 20190725
+ed jk
+
+This is a script to demonstrate the posting of the decay score of a file with
+USACE data into a CARIS BDB database.
+"""
+
+from datetime import datetime
+
+import fuse.fuse_ehydro as ffe
+
+#infilename = 'NB_01_MAI_20160916_CS_4514_30X.XYZ'
+
+if __name__ == '__main__':
+    now = datetime.now()
+    poster = ffe.FuseProcessor_eHydro('cesaj.config')  # this config is local for testing
+    flist = poster._meta_obj.read_meta_file()
+    for f in flist:
+        if 'from_filename' in f:
+            infilename = f['from_filename']
+            poster.score(infilename, now)
+    poster.disconnect()
