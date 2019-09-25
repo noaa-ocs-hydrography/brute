11d812e68719f090803562381e15155c6624e6cd
Casiano Koprowski <casiano.koprowski@noaa.gov>
2019-09-25 12:12:25 -0400

    eHydro_move - Modified size of text field for current region to account for longer region names

--- eHydro_move/eHydro_move_ui.py
+++ eHydro_move/eHydro_move_ui.py
@@ -10,51 +10,51 @@
 import wx
 import wx.xrc
 
-
 ###########################################################################
 ## Class Form
 ###########################################################################
 
-class Form(wx.Frame):
-    """ """
+class Form ( wx.Frame ):
 
-    def __init__(self, parent):
-        wx.Frame.__init__(self, parent, id=wx.ID_ANY, title=u"eHydro_move", pos=wx.DefaultPosition,
-                          size=wx.Size(500, 300), style=wx.DEFAULT_FRAME_STYLE | wx.TAB_TRAVERSAL)
+	def __init__( self, parent ):
+		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = u"eHydro_move", pos = wx.DefaultPosition, size = wx.Size( 500,300 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
 
-        self.SetSizeHints(wx.DefaultSize, wx.DefaultSize)
-        self.SetForegroundColour(wx.SystemSettings.GetColour(wx.SYS_COLOUR_WINDOW))
-        self.SetBackgroundColour(wx.SystemSettings.GetColour(wx.SYS_COLOUR_WINDOW))
+		self.SetSizeHints( wx.DefaultSize, wx.DefaultSize )
+		self.SetForegroundColour( wx.SystemSettings.GetColour( wx.SYS_COLOUR_WINDOW ) )
+		self.SetBackgroundColour( wx.SystemSettings.GetColour( wx.SYS_COLOUR_WINDOW ) )
 
-        bSizer1 = wx.BoxSizer(wx.VERTICAL)
+		bSizer1 = wx.BoxSizer( wx.VERTICAL )
 
-        bSizer3 = wx.BoxSizer(wx.HORIZONTAL)
+		bSizer3 = wx.BoxSizer( wx.HORIZONTAL )
 
-        self.label_region = wx.StaticText(self, wx.ID_ANY, u"Region:", wx.DefaultPosition, wx.DefaultSize, 0)
-        self.label_region.Wrap(-1)
+		bSizer3.SetMinSize( wx.Size( 50,-1 ) )
+		self.label_region = wx.StaticText( self, wx.ID_ANY, u"Region:", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.label_region.Wrap( -1 )
 
-        self.label_region.SetForegroundColour(wx.SystemSettings.GetColour(wx.SYS_COLOUR_INFOTEXT))
+		self.label_region.SetForegroundColour( wx.SystemSettings.GetColour( wx.SYS_COLOUR_INFOTEXT ) )
 
-        bSizer3.Add(self.label_region, 0, wx.ALIGN_CENTER_VERTICAL | wx.ALL, 5)
+		bSizer3.Add( self.label_region, 0, wx.ALIGN_CENTER_VERTICAL|wx.ALL, 5 )
 
-        self.text_region = wx.TextCtrl(self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize,
-                                       wx.TE_CENTER | wx.TE_READONLY)
-        bSizer3.Add(self.text_region, 0, wx.ALIGN_CENTER_VERTICAL | wx.ALL, 5)
+		self.text_region = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, wx.TE_CENTER|wx.TE_READONLY )
+		bSizer3.Add( self.text_region, 1, wx.ALIGN_CENTER_VERTICAL|wx.ALL, 5 )
 
-        bSizer1.Add(bSizer3, 0, 0, 5)
 
-        self.progressBar = wx.Gauge(self, wx.ID_ANY, 100, wx.DefaultPosition, wx.DefaultSize, wx.GA_HORIZONTAL)
-        self.progressBar.SetValue(0)
-        bSizer1.Add(self.progressBar, 0, wx.ALL | wx.EXPAND, 5)
+		bSizer1.Add( bSizer3, 0, wx.EXPAND, 5 )
 
-        self.text_output = wx.TextCtrl(self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize,
-                                       wx.TE_DONTWRAP | wx.TE_MULTILINE | wx.TE_READONLY)
-        bSizer1.Add(self.text_output, 1, wx.ALL | wx.EXPAND, 5)
+		self.progressBar = wx.Gauge( self, wx.ID_ANY, 100, wx.DefaultPosition, wx.DefaultSize, wx.GA_HORIZONTAL )
+		self.progressBar.SetValue( 0 )
+		bSizer1.Add( self.progressBar, 0, wx.ALL|wx.EXPAND, 5 )
 
-        self.SetSizer(bSizer1)
-        self.Layout()
+		self.text_output = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, wx.TE_DONTWRAP|wx.TE_MULTILINE|wx.TE_READONLY )
+		bSizer1.Add( self.text_output, 1, wx.ALL|wx.EXPAND, 5 )
 
-        self.Centre(wx.BOTH)
 
-    def __del__(self):
-        pass
+		self.SetSizer( bSizer1 )
+		self.Layout()
+
+		self.Centre( wx.BOTH )
+
+	def __del__( self ):
+		pass
+
+
