4a46076e5fc0c8888a1feca28c036f5b5dde96a4
GlenRice-NOAA <glen.rice@noaa.gov>
2019-09-27 14:10:38 -0400

    Updating the post script to work on all local configs.  The default location for all configs is now in the subfolder "config_files".  Files should be changed and committed in the subfolder and moved to the post folder when running the post script.

--- fuse_dev/scripts/post/fuse_post.py
+++ fuse_dev/scripts/post/fuse_post.py
@@ -4,19 +4,24 @@ fuse_post.py
 
 grice
 20190703
-V.0.0.1 20190703
+V.0.0.2 20190927
 
 This is a script to demonstrate the posting of a file with USACE data into a
 CARIS BDB database.
+
+All configs in the local directory are run serially.
 """
 
+from glob import glob
 import fuse.fuse_ehydro as ffe
 
 if __name__ == '__main__':
-    poster = ffe.FuseProcessor_eHydro('cenan.config')  # this config is local for testing
-    flist = poster._meta_obj.read_metadata()
-    for f in flist:
-        if 'to_filename' in f:
-            infilename = f['from_filename']
-            poster.post(infilename)
-    poster.disconnect()
+    config_list = glob('*.config')
+    for config in config_list:
+        poster = ffe.FuseProcessor_eHydro('cenan.config')
+        flist = poster._meta_obj.read_metadata()
+        for f in flist:
+            if 'to_filename' in f:
+                infilename = f['from_filename']
+                poster.post(infilename)
+        poster.disconnect()
