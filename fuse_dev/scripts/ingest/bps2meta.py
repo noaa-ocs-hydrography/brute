ff74b639d74d5b6a807af3db32198e87ad2a81d5
Casiano Koprowski <casiano.koprowski@noaa.gov>
2019-11-25 09:02:27 -0500

    scripts/ingest/bps2meta - fixing rogue 'end' kwarg in logging

--- fuse_dev/scripts/ingest/bps2meta.py
+++ fuse_dev/scripts/ingest/bps2meta.py
@@ -55,7 +55,7 @@ if __name__ == '__main__':
             else:
                 for xyz_filename in xyz_filenames:
                     try:
-                        logger.info(f'{datetime.now()}: processing {xyz_filename}', end=', ')
+                        logger.info(f'{datetime.now()}: processing {xyz_filename}')
                         bps_processor.process(xyz_filename)
                     except Exception as error:
                         _, _, error_traceback = sys.exc_info()
